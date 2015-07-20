9fc10d86c9ad041a08500adee7136479fcf52393
Chelsea Worrel <chelseaworrel@gmail.com>
2015-07-20 14:12:54 -0600

    added user table

---
+++ db/migrate/20150720201224_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :user_name
+      t.string :email
+      t.string :provider
+      t.string :token
+      t.string :uid
+      t.string :image_url
+
+      t.timestamps null: false
+    end
+  end
+end
