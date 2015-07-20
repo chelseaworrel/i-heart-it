5a22c360ab72ac2f9421d713979e1c261e63e56a
Chelsea Worrel <chelseaworrel@gmail.com>
2015-07-20 17:48:04 -0600

    removed foundation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :client
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def client
+    @client = Instagram.client(access_token: current_user.token).user
   end
 end
