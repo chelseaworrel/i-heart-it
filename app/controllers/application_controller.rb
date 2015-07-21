5296c4807d174d6ff4623dd1306863c2b97877d7
Chelsea Worrel <chelseaworrel@gmail.com>
2015-07-21 09:50:04 -0600

    added the feed to the index page, including: username, full_name, and profile pic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :client
+  helper_method :current_user, :client, :info
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
@@ -8,5 +8,9 @@ class ApplicationController < ActionController::Base
 
   def client
     @client = Instagram.client(access_token: current_user.token).user
+  end
+
+  def info
+    @info = Instagram.user_recent_media(access_token: current_user.token)
   end
 end
