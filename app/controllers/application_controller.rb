a73a4ee95cdc67ac69303519b69cb02631ea1639
Chelsea Worrel <chelseaworrel@gmail.com>
2015-07-21 16:28:10 -0600

    working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,35 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :client, :user, :current_path, :index_path
+  helper_method :current_user,
+                :path_to_dashboard,
+                :path_to_user,
+                :path_to_root,
+                :client,
+                :user
   # before_action :is_user_present?
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
+  def path_to_user
+    params[:controller] == "users"
+  end
+
+  def path_to_dashboard
+    params[:controller] == "dashboard"
+  end
+
+  def path_to_root
+    params[:controller] == "welcome"
+  end
+
   def client
-    unless session[:user_id]
-      redirect_to root_path
-    end
-    @client = Instagram.client(access_token: current_user.token)
+    @instagram_client ||= Instagram.client(access_token: current_user.token)
   end
 
   def user
     client.user
-  end
-
-  def index_path
-    params[:controller] == "users"
-  end
-
-  def current_path
-    params[:controller] == "dashboard"
   end
 
   # def is_user_present?
