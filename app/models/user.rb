8cafd9f9d94d9831e3f76fafd827ebae9176fb35
Chelsea Worrel <chelseaworrel@gmail.com>
2015-07-23 08:14:21 -0600

    refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,35 +27,20 @@ class User < ActiveRecord::Base
     client.user_recent_media
   end
 
-  def pag
-    response = client.user_media_feed
-    album = [].concat(response)
-    max_id = response.pagination.next_max_id
-
-    while !(max_id.to_s.empty?) do
-        response = client.user_media_feed(:max_id => max_id)
-        max_id = response.pagination.next_max_id
-        album.concat(response)
-    end
-    @album = album
+  def time
+    insta_time = client.user_media_feed.created_time
   end
-end
 
-# get "/user_media_feed" do
-#   client = Instagram.client(:access_token => session[:access_token])
-#   user = client.user
-#   html = "<h1>#{user.username}'s media feed</h1>"
-#
-#   page_1 = client.user_media_feed(777)
-#   page_2_max_id = page_1.pagination.next_max_id
-#   page_2 = client.user_recent_media(777, :max_id => page_2_max_id ) unless page_2_max_id.nil?
-#   html << "<h2>Page 1</h2><br/>"
-#   for media_item in page_1
-#     html << "<img src='#{media_item.images.thumbnail.url}'>"
-#   end
-#   html << "<h2>Page 2</h2><br/>"
-#   for media_item in page_2
-#     html << "<img src='#{media_item.images.thumbnail.url}'>"
-#   end
-#   html
-# end
+  # def pag
+  #   response = client.user_media_feed
+  #   album = [].concat(response)
+  #   max_id = response.pagination.next_max_id
+  #
+  #   while !(max_id.to_s.empty?) do
+  #       response = client.user_media_feed(:max_id => max_id)
+  #       max_id = response.pagination.next_max_id
+  #       album.concat(response)
+  #   end
+  #   @album = album
+  # end
+end
