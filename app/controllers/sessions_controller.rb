59ff11a815dbd6456470628efe9e48008af86170
Chelsea Worrel <chelseaworrel@gmail.com>
2015-08-04 16:30:30 -0600

    welcome page sytled login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     @user = User.find_or_create_from_auth(oauth)
     if @user
       session[:user_id] = @user.id
-      redirect_to users_path
+      redirect_to dashboard_path
     else
       redirect_to root_path
     end
