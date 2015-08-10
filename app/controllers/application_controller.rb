249179205fc6fe96f881593132505c30e7c0c532
Chelsea Worrel <chelseaworrel@gmail.com>
2015-08-10 17:09:49 -0600

    deleted routes I do not need

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method(:current_user, :path_to_dashboard, :path_to_user,:path_to_root)
+  helper_method(:current_user)
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def path_to_user
-    params[:controller] == "users"
-  end
-
-  def path_to_dashboard
-    params[:controller] == "dashboard"
-  end
-
-  def path_to_root
-    params[:controller] == "welcome"
   end
 end
