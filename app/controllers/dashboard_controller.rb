c46359e8615bb4a8ebf2e8d030d846f1a8153680
Chelsea Worrel <chelseaworrel@gmail.com>
2015-08-10 16:52:28 -0600

    refactored code deleted views that I do not need

--- app/controllers/dashboard_controller.rb
+++
@@ -1,9 +0,0 @@
-class DashboardController < ApplicationController
-  def show
-    @pictures = current_user.pictures
-  end
-
-  def destroy
-    redirect_to dashboard_path
-  end
-end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     @user = User.find_or_create_from_auth(oauth)
     if @user
       session[:user_id] = @user.id
-      redirect_to dashboard_path
+      redirect_to user_path(@user)
     else
       redirect_to root_path
     end
