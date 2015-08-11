53f0fa14d9feda72e9bf168cd2dff49ba3300106
Chelsea Worrel <chelseaworrel@gmail.com>
2015-08-11 10:25:25 -0600

    seed data working

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,39 +6,39 @@ Inspiration.create(name: "kidsfurniture",
 Inspiration.create(name: "babyshower",
                    slug: "babyshower",
                   title: "baby shower",
-              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11378662_1009218249111534_982986615_n.jpg"
+              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11378662_1009218249111534_982986615_n.jpg")
 
 Inspiration.create(name: "kidsfashion",
                    slug: "kidsfashion",
                   title: "kids fashion",
-              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11256286_811945082220500_1351987297_n.jpg"
+              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11256286_811945082220500_1351987297_n.jpg")
 
 Inspiration.create(name: "kidsfood",
                    slug: "kidsfood",
                   title: "kids food",
-              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11875366_865954720158501_288508744_n.jpg"
+              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11875366_865954720158501_288508744_n.jpg")
 
 Inspiration.create(name: "kidsofinstagram",
                    slug: "kidsofinstagram",
                   title: "kids 'cheese!'",
-              image_url: "https://scontent.cdninstagram.com/hphotos-xpa1/t51.2885-15/s320x320/e15/11176526_1669509513280234_1829449963_n.jpg"
+              image_url: "https://scontent.cdninstagram.com/hphotos-xpa1/t51.2885-15/s320x320/e15/11176526_1669509513280234_1829449963_n.jpg")
 
 Inspiration.create(name: "kidsparty",
                    slug: "kidsparty",
                   title: "kids party",
-              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11849073_870901959667560_919060123_n.jpg"
+              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11849073_870901959667560_919060123_n.jpg")
 
 Inspiration.create(name: "kidsrooms",
                    slug: "kidsrooms",
                   title: "kids rooms",
-              image_url: "https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s320x320/e15/11821901_1465657153739279_1046864035_n.jpg"
+              image_url: "https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s320x320/e15/11821901_1465657153739279_1046864035_n.jpg")
 
 Inspiration.create(name: "pregnancyfashion",
                    slug: "pregnancyfashion",
                   title: "babybump style",
-              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11363826_1453951351580623_357771235_n.jpg"
+              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11363826_1453951351580623_357771235_n.jpg")
 
 Inspiration.create(name: "kidsdecor",
                    slug: "kidsdecor",
                   title: "kids decor",
-              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11856810_930379657004893_237543586_n.jpg"
+              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11856810_930379657004893_237543586_n.jpg")
