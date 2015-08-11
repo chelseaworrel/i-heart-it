5e84bbf87e3282a8687af18e5c754a6b7ecb90d9
Chelsea Worrel <chelseaworrel@gmail.com>
2015-08-11 10:13:37 -0600

    seed data added for inspiraitons

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Inspiration.create(name: "kidsfurniture",
+                   slug: "kidsfurniture",
+                  title: "kids furniture",
+              image_url: "https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s320x320/e15/11428220_884566301598252_1540025433_n.jpg")
+
+Inspiration.create(name: "babyshower",
+                   slug: "babyshower",
+                  title: "baby shower",
+              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11378662_1009218249111534_982986615_n.jpg"
+
+Inspiration.create(name: "kidsfashion",
+                   slug: "kidsfashion",
+                  title: "kids fashion",
+              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11256286_811945082220500_1351987297_n.jpg"
+
+Inspiration.create(name: "kidsfood",
+                   slug: "kidsfood",
+                  title: "kids food",
+              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11875366_865954720158501_288508744_n.jpg"
+
+Inspiration.create(name: "kidsofinstagram",
+                   slug: "kidsofinstagram",
+                  title: "kids 'cheese!'",
+              image_url: "https://scontent.cdninstagram.com/hphotos-xpa1/t51.2885-15/s320x320/e15/11176526_1669509513280234_1829449963_n.jpg"
+
+Inspiration.create(name: "kidsparty",
+                   slug: "kidsparty",
+                  title: "kids party",
+              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11849073_870901959667560_919060123_n.jpg"
+
+Inspiration.create(name: "kidsrooms",
+                   slug: "kidsrooms",
+                  title: "kids rooms",
+              image_url: "https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s320x320/e15/11821901_1465657153739279_1046864035_n.jpg"
+
+Inspiration.create(name: "pregnancyfashion",
+                   slug: "pregnancyfashion",
+                  title: "babybump style",
+              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11363826_1453951351580623_357771235_n.jpg"
+
+Inspiration.create(name: "kidsdecor",
+                   slug: "kidsdecor",
+                  title: "kids decor",
+              image_url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11856810_930379657004893_237543586_n.jpg"
